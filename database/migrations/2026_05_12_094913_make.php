e00b20fb00cc26f8f950f39e04b86c4ad2468834
Salim-ghazouli <slim.ghazouli@gmail.com>
2026-05-12 12:50:13 +0300

    task 4 complete

--- database/migrations/2026_05_12_094913_make.php
+++ database/migrations/2026_05_12_094913_make.php
@@ -11,13 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('course_user', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('course_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->timestamps();
-        });
-
         Schema::table('courses', function (Blueprint $table) {
             $table->dropForeign(['instructor_id']);
             $table->dropColumn('instructor_id');
